927c6405e5fa9304cea3d3d6fb5605f1a9c87cab
Matt Chapman <matt@mattchapman.io>
2021-12-05 13:51:09 +1300

    Update

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,45 +1,64 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] int loadDelay = 1;
+    [SerializeField] AudioClip winSound;
+    [SerializeField] AudioClip loseSound;
+    
+    private AudioSource _audioSource;
     private int _activeLevelIndex;
     private int _nextLevelIndex;
 
     void Start()
     {
+        _audioSource = gameObject.GetComponent<AudioSource>();
         _activeLevelIndex = SceneManager.GetActiveScene().buildIndex;
         _nextLevelIndex = _activeLevelIndex.Equals(SceneManager.sceneCountInBuildSettings - 1)
             ? 0
             : _activeLevelIndex + 1;
     }
-
-    void Update()
-    {
-    }
-
+    
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("You hit a friendly object!");
-                break;
-            case "Fuel":
-                Debug.Log("You picked up fuel!");
                 break;
             case "Finish":
-                Debug.Log("You win!");
-                LoadNextLevel();
+                StartSuccessSequence();
                 break;
             default:
-                Debug.Log("You hit an obstacle.");
-                ReloadLevel();
+                StartCrashSequence();
                 return;
         }
     }
 
+    private void StartSuccessSequence()
+    {
+        StopPlayerInteraction();
+        PlaySound(winSound);
+        Invoke(nameof(LoadNextLevel), loadDelay);
+    }
+    
+    private void StartCrashSequence()
+    {
+        StopPlayerInteraction();
+        PlaySound(loseSound);
+        Invoke(nameof(ReloadLevel), loadDelay);
+    }
+
+    private void PlaySound(AudioClip audioClip)
+    {
+        _audioSource.PlayOneShot(audioClip);
+    }
+
+    private void StopPlayerInteraction()
+    {
+        GetComponent<Movement>().enabled = false;
+    }
+    
     private void LoadNextLevel()
     {
         SceneManager.LoadScene(_nextLevelIndex);
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float mainThrustFactor = 100f;
     [SerializeField] private float rotationThrustFactor = 50f;
-    [SerializeField] private AudioClip mainEngineSound;
+    [SerializeField] AudioClip mainEngineSound;
 
     private Rigidbody _rigidbody;
     private AudioSource _audioSource;
@@ -28,18 +26,17 @@ public class Movement : MonoBehaviour
 
     private void ProcessThrust()
     {
-        if (!Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.UpArrow))
-        {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
             _audioSource.Stop();
+        
+        if (!Input.GetKey(KeyCode.UpArrow))
             return;
-        }
 
         if (!_audioSource.isPlaying)
         {
             _audioSource.PlayOneShot(mainEngineSound);
         }
 
-        Debug.Log("Space pressed");
         ApplyThrust(Vector3.up, mainThrustFactor);
     }
 
@@ -47,14 +44,12 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Left pressed");
             ApplyRotationThrust(Vector3.forward);
             return;
         }
 
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Right pressed");
             ApplyRotationThrust(Vector3.back);
             return;
         }
